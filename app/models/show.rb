b43f9e3b557e10c0a727f74694ee15bb5e68bccd
Jason Baik <baikjs87@gmail.com>
2022-08-04 23:51:17 -0400

    complete

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+    
+  def actors_list
+    actor = self.actors
+    actor.first.first_name + " " + actor.first.last_name
+  end
 end
